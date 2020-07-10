a1395df43add81db3888c2e594c03b73b735cee8
Marcus Moreno <legendary.1198@gmail.com>
2020-07-10 04:15:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,12 @@
 def reverse_each_word(phrase)
  
-  flip = phrase.split("").collect do |word|
+  reversed = phrase.split("").collect do |word|
     
     word.reverse
     
   end
   
   
-  flip.join("")
+  reversed.join("")
+  
 end
